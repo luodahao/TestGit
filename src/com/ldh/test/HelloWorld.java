3ad3fe2c06dae050ec9ac91ac88bff1d4f11083b
ldahao <luo.dahao@163.com>
2018-11-20 11:57:26 +0800

    first commit

---
+++ src/com/ldh/test/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.ldh.test;
+
+/**
+ * @author ldahao
+ * @create 2018/11/20
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World!!!");
+    }
+}
